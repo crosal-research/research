e3fe03450aa82e579376cd39fdefe2b2e0145fdf
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-06-28 15:09:38 -0300

    add plotly boilerplace to interest_spread_chart.py

---
+++ monetary_policy/interest_spread_chart.py
@@ -0,0 +1,8 @@
+######################################################################
+# Chart for interest rates sprads
+# initial date: 28/06/2016
+######################################################################
+
+import pandas as pd
+import plotly.plotly as py
+import plotly.graph_objs as go
