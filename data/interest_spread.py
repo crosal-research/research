c75b99cfdf6925a4171c5dab6bddcda77ff3c060
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-06-28 14:47:49 -0300

    modificafy data/interest_spread.py to download data and save as csv

--- data/interest_spread.py
+++ data/interest_spread.py
@@ -4,7 +4,12 @@
 ######################################################################
 import finnacloud_api as fa
 import json, os
+import pandas as pd
 
 f_dir = os.path.join(os.path.expanduser('~'),"crosal/.finnacloud.json")
 key = json.loads(open(f_dir).read())['key'].encode('utf-8')
 fc = fa.Finn_Api(key)
+
+series = ['bcb.selic', 'bcb.cdi6m','bcb.cdi1y']
+df = fc.get_series(series)
+df.to_csv('./interest_spread.csv', header=True, index=True)
