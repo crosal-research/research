e9d627e1bc04bb77752bbf42f07c22a6301a016d
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-01 14:52:59 -0300

    add series and annotation to monetary policy chart

--- monetary_policy/mom_policy_map_chart.py
+++ monetary_policy/mom_policy_map_chart.py
@@ -15,12 +15,21 @@ df_final.columns = ['expected', 'ipca', 'core', 'target', 'lower', 'upper', 'sup
 
 
 ## chart
-trace01 = go.Scatter(x=df.index, y=df['ipca'], name="CPI Inflation")
-trace01 = go.Scatter(x=df.index, y=df['target'], name="Target")
-trace01 = go.Scatter(x=df.index, y=df['core'], name="Core CPI")
-data = [trace01, trace02, trace03]
+trace01 = go.Scatter(x=df_final.index, y=df_final['ipca'],
+                     name="CPI Inflation")
+trace02 = go.Scatter(x=df_final.index, y=df_final['target'],
+                     name="Target")
+trace03 = go.Scatter(x=df_final.index, y=df_final['core'],
+                     name="Core CPI")
+trace04 = go.Scatter(x=df_final.index, y=df_final['expected'],
+                      name="Expeted CPI - One year")
+trace05 = go.Scatter(x=df_final.index, y=df_final['upper'],
+                     name="Upper Limit", line=dict(color="red", dash="dot"))
+trace06 = go.Scatter(x=df_final.index, y=df_final['lower'],
+                     name="Lower Limit", line=dict(color="red", dash="dot"))
+data = [trace01, trace02, trace03, trace04, trace05, trace06]
 layout = go.Layout(title="<b>Monetary Policy Credibility Map</b>",
                    yaxis=dict(title="%p.y.", tickmode='auto', nticks=5),
-                   fontsize=dict(size=12), legend=dict(x=0, y=-0.4))
+                   font=dict(size=12), legend=dict(x=0, y=-0.4))
 fig = go.Figure(data=data, layout=layout)
-py.image.save_as(fig, 'monetary_pol_map.jpeg', format=jpeg)
+py.image.save_as(fig, 'monetary_pol_map.jpeg', format="jpeg")
