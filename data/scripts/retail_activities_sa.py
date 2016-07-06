597242c1a55b0b32a0a8f960b4d68f33d1f45137
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-06 19:53:25 -0300

    add script to fetch data on retail activities sa fro sidra's api

---
+++ data/scripts/retail_activities_sa.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+
+# #####################################################################
+# script to fetch data from sidra's api on retail (sa data)
+# initial date: 06/07/2016
+######################################################################
+from ibge import *
+import pandas as pd
+
+## atividades
+url = "http://www.sidra.ibge.gov.br/api/values/t/3418" + \
+      "/p/all/v/564/c11046/12825/c85/{}/n1/1/f/a"
+
+d = {"90671": "Combustíveis e lubrificantes",
+     "90672":  "Hipermercados, supermercados, produtos alimentícios, bebidas e fumo",
+     "103154":  "Hipermercados e supermercados",
+     "90673":  "Tecidos, vestuário e calcados",
+     "2759":  "Móveis e eletrodomesticos",
+     "31555":  "Móveis",
+     "31556":  "Eletrodomesticos",
+     "103155":  "Artigos farmaceuticos, medicos, ortopedicos, de perfumaria e cosmeticos",
+     "103156":  "Livros, jornais, revistas e papelaria",
+     "103157":  "Equipamentos e materiais para escritório, informática e comunicacao",
+     "103158":  "Outros artigos de uso pessoal e domestico"}
+
+series = ['31556',
+          '31555',
+          '103158',
+          '103156',
+          '103157',
+          '103154',
+          '103155',
+          '90673',
+          '90672',
+          '90671',
+          '2759']
+
+names = [d[k] for k in series]
+urls = [url.format(s) for s in series]
+
+df = ibge_fetch(urls)
+df.columns = names
+df.index.name = "Date"
+
+
+## total, others and contruction
+url1 = "http://www.sidra.ibge.gov.br/api/values/t/3416" + \
+       "/p/all/v/564/c11046/12825/n1/1/f/a"
+url2 = "http://www.sidra.ibge.gov.br/api/values/t/3420" + \
+       "/p/all/v/568/c11046/12825/n1/1/f/a"
+url3 = "http://www.sidra.ibge.gov.br/api/values/t/3415" + \
+       "/p/all/v/1195/c11046/12825/n1/1/f/a"
+
+urls1 = [url1, url2, url3]
+names = ["Total", "Auto and Motorbikes", "Construction"]
+
+df1 = ibge_fetch(urls1)
+df1.columns = names
+df1.index.name = "Date"
+
+## Data Consolidation and save onto to the disk
+df_final = pd.merge(df, df1, left_index=True, right_index=True, how="outer")
+df_final.to_csv("../retail_activities_sa.csv", index=[0], header=True)
