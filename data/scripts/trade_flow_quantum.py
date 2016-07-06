4d33841e5345c4e192b254e706081e701c7e7847
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-06 16:09:18 -0300

    script to fetch data on import and export quantum

---
+++ data/scripts/trade_flow_quantum.py
@@ -0,0 +1,15 @@
+######################################################################
+# script to fetch data trade flow's volume for Brazil from ipeadata
+# initial date: 07/07/2016
+######################################################################
+import pandas as pd
+
+## url for the data
+url = "http://www.ipeadata.gov.br/ExibeSerie.aspx?oper=exportCSVBr&"
+url = url + "serid35690=35690&serid35590=35590"
+df = pd.read_csv(url, sep = ";", usecols=[0, 1, 2],
+                 parse_dates=[0], decimal=",", index_col=0).dropna()
+df.columns = ['import', 'export']
+
+## save data
+df.to_csv('../trade_flow_quantum.csv', index=True, header=True)
