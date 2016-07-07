e11153855785009f518ca092b7fa67c60f626dcb
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-07 19:28:27 -0300

    modify unit_labor_cost_us.py file to retrive manufactoring labor cost only

--- data/scripts/unit_labor_cost_us.py
+++ data/scripts/unit_labor_cost_us.py
@@ -2,30 +2,13 @@
 # fetch data for unit labor cost, Brazil
 # initial date: 04/07/2016
 ######################################################################
-import finnacloud_api as fa
-import json, os
-import pandas as pd
+from fred import *
 
 
-f_dir = os.path.join(os.path.expanduser('~'),"crosal/.finnacloud.json")
-key = json.loads(open(f_dir).read())['key'].encode('utf-8')
-fc = fa.Finn_Api(key)
+series = ['ULCMFG']
 
-
-series = ['FRED.ULCNFB']
-df = fc.get_series(series)
+df = fetch_fred(series)
 df.columns = ["cut"]
 
-# correction on no-existing date
-dates = []
-for x in df.index:
-    if pd.to_datetime(x).month in [1, 4, 7, 10]:
-        dates.append(True)
-    else:
-        dates.append(False)
-
-
-df_final = df[dates]
-
-# save onto the disk
-df_final.to_csv("../cut_us.csv", header=True, index=True)
+## save onto the disk
+df.to_csv("../cut_us.csv", header=True, index=True)
