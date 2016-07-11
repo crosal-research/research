e02044a5dcfae90ad0829d75ee8fcb7d92f8b42f
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-11 19:31:52 -0300

    draw chart for total retail sale mom sa

--- activity_brazil/retail_activities_sa_chart.py
+++ activity_brazil/retail_activities_sa_chart.py
@@ -7,4 +7,25 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 
 # fetch data
-df = pd.read_csv("../data/retail_activities_sa.csv", index_col=[0], header=[0])
+df = pd.read_csv("../data/retail_activities_sa.csv", index_col=[0])
+df_mave = df.rolling(window=3).mean()
+
+# total
+df_total = pd.merge(pd.DataFrame(df['Total']), pd.DataFrame(df_mave['Total']),
+                    left_index=True,  right_index=True,
+                    how="inner").pct_change(periods=1) * 100
+df_total.columns = ["total", "ma"]
+
+start_date = "2015-09-01"
+df_total_new = df_total[df_total.index >= start_date]
+
+## chart
+trace01 = go.Scatter(x=df_total_new.index,
+                     y = df_total_new['total'], name = "Total")
+trace02 = go.Bar(x=df_total_new.index,
+                     y = df_total_new['ma'], name = "Moving Average")
+data = [trace01, trace02]
+layout = go.Layout(title="<b>Retail Sales</b>", legend=dict(x=0, y=-0.4),
+                   font=dict(size=18), yaxis=dict(title="%", tickmode="auto", nticks=5))
+fig = go.Figure(data=data, layout=layout)
+py.image.save_as(fig, "../exhibits/retail_total_chart.jpeg", format="jpeg")
