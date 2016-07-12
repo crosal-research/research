7ceea9a42c32ec54aea0179e00f34eb9e40a3c96
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-12 11:29:08 -0300

    add annotation to total retail sales chart

--- activity_brazil/retail_activities_sa_chart.py
+++ activity_brazil/retail_activities_sa_chart.py
@@ -10,21 +10,30 @@ import plotly.graph_objs as go
 df = pd.read_csv("../data/retail_activities_sa.csv", index_col=[0])
 df_mave = df.rolling(window=3).mean()
 
-# total
+### total sales
 df_total = pd.merge(pd.DataFrame(df['Total']), pd.DataFrame(df_mave['Total']),
                     left_index=True,  right_index=True,
                     how="inner").pct_change(periods=1) * 100
 df_total.columns = ["total", "ma"]
 
-start_date = "2015-09-01"
+df_total_last = df_total.iloc[-1:, :]
+start_date = "2015-10-01"
 df_total_new = df_total[df_total.index >= start_date]
 
-## chart
+
+# chart
 trace01 = go.Scatter(x=df_total_new.index,
                      y = df_total_new['total'], name = "Total")
 trace02 = go.Bar(x=df_total_new.index,
                      y = df_total_new['ma'], name = "Moving Average")
-data = [trace01, trace02]
+trace03 = go.Scatter(x=df_total_last.index, y=df_total_last['total'],
+                    mode="markers+text",
+                    text=["<b>"+str(round(df_total_last.values[0][0]))+"</b>"],
+                     textposition='leftl', showlegend=False)
+
+data = [trace01, trace02, trace03]
+
+
 layout = go.Layout(title="<b>Retail Sales</b>", legend=dict(x=0, y=-0.4),
                    font=dict(size=18), yaxis=dict(title="%", tickmode="auto", nticks=5))
 fig = go.Figure(data=data, layout=layout)
