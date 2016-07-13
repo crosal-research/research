8376f4349f49012e0a1d718a4d0578cadd5819b2
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-13 15:44:54 -0300

    script to retrieve data on trade balance from bcb webservice

---
+++ data/scripts/trade_balance.py
@@ -0,0 +1,17 @@
+######################################################################
+# script to download balance of payment data from BCB's webservices
+# initial date: 13/07/2016
+######################################################################p
+import pandas as pd
+from bcb import *
+
+
+series = ['22701', '22702', '22703', '22707', '22708', '22709', '22719', '22720',
+          '22721', '4385']
+names = ['current_account', 'cc_reveneus', 'cc_spending', 'trade_balance',
+         'good_exports', 'good_imports', 'serv_balance', 'serv_exports',
+         'serv_imports', 'GDP']
+
+df = fetch_bcb(series, '01/01/1995', '01/05/2016')
+df.columns = names
+df.to_csv('../trade_balance.csv', header=True, index=True)
