420d7690d436b1cb8e7daf595daf3f1ea6d894cc
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-14 17:57:00 -0300

    add script to retrieve data on credit quality for Brazil from BCB webservice

---
+++ data/scripts/credit_quality.py
@@ -0,0 +1,17 @@
+######################################################################
+# retrieves credit quality info on non-earmarked from BCB webservices
+# initial date: 14/07/2016
+######################################################################p
+import pandas as pd
+from bcb import *
+
+series = {"21085":"total_del", "21086":"companies_del", "21112": "households_del",
+          "21006": "total_late", "21007": "companies_late", "21008": "households_late",
+          "20717": "total_juros", "20718":"companies_juros", "20740": "households_juros",
+          "20786": "total_spread", "20787": "companies_spread", "20809": "households_spread"}
+
+df = fetch_bcb(series.keys(), '01/03/2011', '01/05/2016')
+
+
+df.columns = [series[s] for s in df.columns]
+df.to_csv('../credit_quality.csv', header=True, indexo=True)
