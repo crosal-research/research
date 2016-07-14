856156b552bce8bcd4910a66357d5b37b40d9999
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-14 20:19:44 -0300

    fix end of period time to allways fetch the lastes date

--- data/scripts/credit_volume.py
+++ data/scripts/credit_volume.py
@@ -3,12 +3,14 @@
 # initial date: 14/07/2016
 ######################################################################p
 import pandas as pd
+from datetime import datetime
 from bcb import *
 
 series = {'20539':"Total", '20540':"Institutions",
           '20541': "Households", '20542':"non-earmarked", '20593':"earmarked",
           '20594':"earmarked_com", '4380':"GDP"}
 
+today = datetime.today().strftime("%d/%m/%Y")
 df = fetch_bcb(series.keys(), '01/03/2006', '01/05/2016')
 
 
