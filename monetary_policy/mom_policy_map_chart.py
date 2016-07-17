47e975e7191dba0b8b38d55dd25a1909e6918eec
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-17 00:31:36 -0300

    fix color, legend and names of series on the char monetary policy credibility map

--- monetary_policy/mom_policy_map_chart.py
+++ monetary_policy/mom_policy_map_chart.py
@@ -10,26 +10,32 @@ import plotly.graph_objs as go
 ## fetch data
 df_exp = pd.read_csv('../data/expected.csv', index_col = 0, decimal=",", parse_dates=[0])
 df_inf = pd.read_csv('../data/mom_policy_map.csv', index_col=0)
-df_final = pd.merge(df_exp, df_inf, left_index=True, right_index=True, how = "inner")
-df_final.columns = ['expected', 'ipca', 'core', 'target', 'lower', 'upper', 'sup']
+df_mid = pd.merge(df_exp, df_inf, left_index=True, right_index=True, how = "inner")
+df_mid.columns = ['expected', 'ipca', 'core', 'target', 'lower', 'upper']
+
+
+new_date = "2010-01-01"
+df_final = df_mid[df_mid.index >= new_date]
 
 
 ## chart
 trace01 = go.Scatter(x=df_final.index, y=df_final['ipca'],
-                     name="CPI Inflation")
+                     name="CPI Inflation", line=dict(color="orange"))
 trace02 = go.Scatter(x=df_final.index, y=df_final['target'],
-                     name="Target")
+                     name="Target", line=dict(color="red"))
 trace03 = go.Scatter(x=df_final.index, y=df_final['core'],
                      name="Core CPI")
 trace04 = go.Scatter(x=df_final.index, y=df_final['expected'],
-                      name="Expeted CPI - One year")
+                      name="Expeted CPI - One year", line=dict(color="black"))
 trace05 = go.Scatter(x=df_final.index, y=df_final['upper'],
-                     name="Upper Limit", line=dict(color="red", dash="dot"))
+                     name="Upper Limit", line=dict(color="red", dash="dot"),
+                     showlegend=False)
 trace06 = go.Scatter(x=df_final.index, y=df_final['lower'],
-                     name="Lower Limit", line=dict(color="red", dash="dot"))
+                     name="Lower Limit", line=dict(color="red", dash="dot"),
+                     showlegend=False)
 data = [trace01, trace02, trace03, trace04, trace05, trace06]
 layout = go.Layout(title="<b>Monetary Policy Credibility Map</b>",
-                   yaxis=dict(title="%p.y.", tickmode='auto', nticks=5),
-                   font=dict(size=12), legend=dict(x=0, y=-0.4))
+                   yaxis=dict(title="%mom", tickmode='auto', nticks=5),
+                   font=dict(size=18), legend=dict(x=0, y=-0.6))
 fig = go.Figure(data=data, layout=layout)
 py.image.save_as(fig, '../exhibits/monetary_pol_map.jpeg', format="jpeg")
