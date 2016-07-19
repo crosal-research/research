d21a7876525c25ac32fd28bf1dd9435b95178f3f
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-19 15:18:52 -0300

    change mon_poply_map to retrive data from bcb rather than finnacloud

--- data/scripts/mon_policy_map.py
+++ data/scripts/mon_policy_map.py
@@ -2,20 +2,22 @@
 # Script to download data for monetary policy map
 # initial date: 30/06/2016
 ######################################################################
-import finnacloud_api as fa
+from bcb import *
 import json, os
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
-f_dir = os.path.join(os.path.expanduser('~'),"crosal/.finnacloud.json")
-key = json.loads(open(f_dir).read())['key'].encode('utf-8')
-fc = fa.Finn_Api(key)
+series = {"433": "ipca", "4466": "core", "13521": "target"}
+today = datetime.today().strftime("%d/%m/%Y")
 
 
-series = ["bcb.ipcam", 'BCB.BRIPCACORESMAVEM', 'BCB.BRINFTARGET']
-df = fc.get_series(series)
-df.columns = ["ipca", 'core', 'target']
-df['target'][-1] = 4.5
+df_ipca = fetch_bcb(["433", "4466"], "01/01/1993",today)
+df_target = fetch_bcb(["13521"], "01/01/1993",today)
+df = pd.merge(df_ipca, df_target, left_index=True,
+                  right_index=True, how="outer").fillna(method="pad")
+df.columns = [series[s] for s in df.columns]
+
 
 
 # series manipulations
@@ -23,12 +25,14 @@ df_inf = df[['ipca', 'core']].dropna()
 df_inf12 = (1+df_inf/100).cumprod().pct_change(periods=12)*100
 
 # target
-df_center = pd.DataFrame(df['target'].fillna(method="pad"))
+df_center = pd.DataFrame(df['target'])
 df_upper = df_center + 2.0
 df_lower = df_center - 2.0
 
-df_target = pd.merge(df_center, df_lower, left_index=True, right_index=True, how='inner')
-df_target = pd.merge(df_target, df_upper, left_index=True, right_index=True, how='inner').dropna()
+df_target = pd.merge(df_center, df_lower, left_index=True,
+                     right_index=True, how='inner')
+df_target = pd.merge(df_target, df_upper, left_index=True,
+                     right_index=True, how='inner').dropna()
 
 # final
 start_date = "2000-01-01"
