81550990c16f208f5b6ee512f74e28af69d558b7
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-19 14:19:01 -0300

    render chart of credit valume through a function

--- credit_brazil/credit_volume.py
+++ credit_brazil/credit_volume.py
@@ -11,17 +11,23 @@ df_ch = df.pct_change(periods=12)*100
 df_12m = df.rolling(window=12).sum().dropna()
 df_gdp = df_12m.div(df_12m['GDP'], axis=0).iloc[:,1:]*10
 
-new_date = "2010-01-01"
-df_gdp_new = df_gdp[df_gdp.index >= new_date]
 
-## volume
-trace01 = go.Scatter(x=df_gdp_new.index, y=df_gdp_new['Total'], name="Total")
-trace02 = go.Scatter(x=df_gdp_new.index, y=df_gdp_new['Institutions'], name="Institutions")
-trace03 = go.Scatter(x=df_gdp_new.index, y=df_gdp_new['Households'], name="Households")
-data0 = [trace01, trace02, trace03]
-layout0 = go.Layout(title="<b>Credit Volume</b>",
-                    yaxis=dict(title="%GDP", tickmode="auto", nticks=5),
-                    font=dict(size=18),
-                    legend=dict(x=0, y=-0.4))
-fig = go.Figure(data=data0, layout=layout0)
-py.image.save_as(fig, '../exhibits/credit_gdp_chart.jpeg', format="jpeg")
+def credit_volume(df_gdp, new_date, title):
+    df_gdp_new = df_gdp[df_gdp.index >= new_date]
+    names = df_gdp.columns
+    ## volume
+    trace01 = go.Scatter(x=df_gdp_new.index, y=df_gdp_new['Total'],
+                         name=names[0])
+    trace02 = go.Scatter(x=df_gdp_new.index, y=df_gdp_new['Institutions'],
+                         name=names[1])
+    trace03 = go.Scatter(x=df_gdp_new.index, y=df_gdp_new['Households'],
+                         name=names[2])
+    data0 = [trace01, trace02, trace03]
+    layout0 = go.Layout(title="<b>{}</b>".format(title),
+                        yaxis=dict(title="%GDP", tickmode="auto", nticks=5),
+                        font=dict(size=18),
+                        legend=dict(x=0, y=-0.4))
+    return go.Figure(data=data0, layout=layout0)
+
+py.image.save_as(credit_volume(df_gdp, "2010-01-01", "Credit Volume - Brazil" ),
+                 '../exhibits/credit_gdp_chart.jpeg', format="jpeg")
