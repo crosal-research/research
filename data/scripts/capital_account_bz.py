e211f8fe3a4e9e6ab3ceabc05a03e122c19f8047
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-22 00:50:35 -0300

    add script to retreive data on capital account and reservers for Brazil from bcb webservice

---
+++ data/scripts/capital_account_bz.py
@@ -0,0 +1,24 @@
+######################################################################
+# script to download capital account data from BCB's webservices
+# initial date: 21/07/2016
+# comments:
+######################################################################p
+import pandas as pd
+from bcb import *
+from datetime import datetime
+
+
+
+series = {'22863':"net_financial_account", '22864':'net_fdi', '22905':"net_portfolio_flow",
+          '22966':"net_derivatives", '22969':"net_others"}
+
+series_fx ={"13982":"fx_reserves"}
+
+today = datetime.today().strftime("%d/%m/%Y")
+
+df = fetch_bcb(series.keys(), '01/01/1995', today)
+df_fx = fetch_bcb(series_fx.keys(), '01/01/2008', today)
+df.columns = [series[n] for n in df.columns]
+df_fx.columns = [series_fx[n] for n in df_fx.columns]
+df.to_csv('../net_capital_flow.csv', header=True, index=True)
+df_fx.to_csv('../fx_reserves.csv', header=True, index=True)
