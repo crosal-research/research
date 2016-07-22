e2dabaa8799cc970d14126d9154624e22bf150ac
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-22 00:11:12 -0300

    add script to draw charts on capital account bz

---
+++ external_sector/capital_account_chart_bz.py
@@ -0,0 +1,7 @@
+######################################################################
+# draw data on capital account data
+# initial date: 21/07/2016
+######################################################################
+import pandas as pd
+import plotly.plotly as py
+import plotly.graph_objs as go
