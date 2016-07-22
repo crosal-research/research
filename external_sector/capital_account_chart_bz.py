d69ab5eb3ec216e63c7dd5b723a4ab70e3edd6f2
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-07-22 00:40:39 -0300

    draw first version of chart on net financial inflow

--- external_sector/capital_account_chart_bz.py
+++ external_sector/capital_account_chart_bz.py
@@ -5,3 +5,25 @@
 import pandas as pd
 import plotly.plotly as py
 import plotly.graph_objs as go
+
+df = pd.read_csv("../data/net_capital_flow.csv", index_col=0)
+
+
+## chart
+def net_flow(newdate, series, title, ytitle):
+    df_new = df[df.index >= newdate]
+    df_sum = df.rolling(window=12).mean()
+    df_sum_new = df_sum[df_sum.index >= newdate]
+    trace01 = go.Scatter(x=df_new.index, y=df_new[series[0]],
+                         name="Monthly")
+    trace02 = go.Scatter(x=df_sum_new.index, y=df_sum_new[series[0]],
+                         name="12M rolling average", line=dict(dash="dot"))
+    data0 = [trace01, trace02]
+    layout0 = go.Layout(title="<b>{}</b>".format(title),
+                        yaxis=dict(title=ytitle, tickmode="auto", nticks=5),
+                        font=dict(size=18), legend=dict(x=0, y=-0.4))
+    return go.Figure(data=data0, layout=layout0)
+
+py.image.save_as(net_flow("2010-01-01", ["net_financial_account"], "Net Financial Account",
+                          ytitle="USDmn"),
+                "../exhibits/net_capital_flow.jpeg",format="jpeg")
