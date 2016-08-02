0bd657d69b4d7b90566bacd58ae023c6a3c39ecf
U-DESKTOP-7R25MLQ\jmros <jmrosal@crosal-research.com->
2016-08-02 17:29:53 -0300

    add script to fetch data from stock markets

---
+++ data/scripts/stock_markets.py
@@ -0,0 +1,16 @@
+######################################################################
+# fetch stock market data from yahoo
+#
+######################################################################
+from yahoo import *
+import pandas as pd
+from datetime import datetime
+
+
+series=["^BVSP", "GSPC"]
+date_ini = "01/01/2011"
+today = datetime.today().strftime("%m/%d/%Y")
+
+df = fetch_yahoo(series,date_ini, today)
+
+df.to_csv("../stock_markets.csv", header=True, index=True)
