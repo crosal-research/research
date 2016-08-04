219dedf80f04832ebf487951bb67f2f8ae750f8c
jmrosal <jmrosal@crosal-research.com>
2016-08-04 12:57:13 -0300

    add 2 years treasury to the file interest_rates_us.py

--- data/scripts/interest_rates_us.py
+++ data/scripts/interest_rates_us.py
@@ -6,7 +6,7 @@ from fred import *
 import pandas as pd
 
 
-series = ["DGS10", "DGS5", "DFF"]
+series = ["DGS10", "DGS5", "DGS2", "DFF"]
 df = fetch_fred(series)
 
 df.to_csv("../interest_rates_us.csv", index=[0], header=True)
