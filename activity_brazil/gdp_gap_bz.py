5fcedbfbb6bdc66d9524ae2337a190761a356212
jmrosal <jmrosal@crosal-research.com>
2016-08-04 06:16:29 -0300

    add notation to output gap chart

--- activity_brazil/gdp_gap_bz.py
+++ activity_brazil/gdp_gap_bz.py
@@ -10,17 +10,22 @@ import statsmodels.api as sm
 
 df = pd.read_csv("../data/gdp_sa_categories.csv", index_col=0)
 df_gdp = df.iloc[:, 1]
-cf_cycle, cf_trend =sm.tsa.filters.cffilter(df_gdp,
-                                            low=4,
-                                            high=24,
-                                            drift=True)
+cf_cycle, cf_trend = sm.tsa.filters.cffilter(df_gdp,
+                                             low=4,
+                                             high=24,
+                                             drift=True)
 
 ##draw chart for GDP gap
 def gap_fig(df_input, title, y_title, date_ini):
     df = df_input[df_input.index >= date_ini]
+    df_last = df.tail(1)
     trace01 = go.Bar(x=df.index, y=df,
-                     name= title)
-    data = [trace01]
+                     name=title)
+    trace02 = go.Scatter(x=df_last.index, y=df_last,
+                         mode="markets+text",
+                         text=["<b>"+str(round(df_last.values[0]))+"</b>"],
+                         textposition='bottom', showlegend=False)
+    data = [trace01, trace02]
     layout = go.Layout(title="<b>{}</b>".format(title),
                        font=dict(size=18),
                        yaxis=dict(title="{}".format(y_title)))
