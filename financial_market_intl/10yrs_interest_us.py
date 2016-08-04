8dfab9013b3b15306c6e5c0953ef8934aa4218a8
jmrosal <jmrosal@crosal-research.com>
2016-08-04 13:07:41 -0300

    re-write file has been unduly deleted

--- financial_market_intl/10yrs_interest_us.py
+++ financial_market_intl/10yrs_interest_us.py
@@ -1,4 +1,4 @@
-######################################################################
++######################################################################
 # script to draw chart of 10 years us treasury
 # initial data: 08/07/2016
 ######################################################################
@@ -9,14 +9,26 @@ import plotly.graph_objs as go
 df = pd.read_csv('../data/interest_rates_us.csv', index_col=[0],
                  usecols=[0,1], na_values=["NaN", "."]).dropna()
 
-start_date = "2005-01-01"
-df_final = df[df.index >= start_date]
 
 
 ## chart
-trace01 = go.Scatter(x=df_final.index, y=df_final['DGS10'])
-data = [trace01]
-layout = go.Layout(title="<b>Ten Years Treasury</b>", font=dict(size=18),
-                   yaxis=dict(title="% p.y", tickmode="auto", nticks=5))
-fig = go.Figure(data=data, layout=layout)
-py.image.save_as(fig, "../exhibits/10yrs_interest_us.jpeg", format="jpeg")
+
+def chart_gen(df, title, y_title, leg, data_ini):
+    df_final = df[df.index >= data_ini]
+    trace01 = go.Scatter(x=df_final.index, y=df_final.iloc[:, 0],
+                         name=leg[0], showlegend=False)
+    data = [trace01]
+    layout = go.Layout(title="<b>{}</b>".format(title),
+                       font=dict(size=18),
+                       yaxis=dict(title="{}".format(y_title),
+                                  tickmode="auto", nticks=5),
+                       legend=dict(x=0, y=-0.4))
+    return go.Figure(data=data, layout=layout)
+
+
+# generates charts
+start_date = "2005-00-01"
+py.image.save_as(chart_gen(df, "10 Years Treasury", "%annual",
+                           ["10 Years Treasury"],
+                           start_date),
+                 "../exhibits/10yrs_interest_us.jpeg", format="jpeg")
