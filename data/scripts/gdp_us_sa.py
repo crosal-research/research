9cb922f2e87399683411afb29c7804c7bb37f9e5
jmrosal <jmrosal@crosal-research.com>
2016-08-04 16:43:29 -0300

    add script to add gdp data for us

---
+++ data/scripts/gdp_us_sa.py
@@ -0,0 +1,10 @@
+######################################################################
+# script to fetch gdp data for us from fred
+# intial date: 08/4/2016
+######################################################################
+import pandas as pd
+from fred import *
+
+series = ["GDP"]
+df = fetch_fred(series)
+df.to_csv("../gdp_us_sa.csv", header=True, index=True)
