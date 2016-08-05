5c1654ac335e3ab3a3fb01361220f257aa89cfe9
jmrosal <jmrosal@crosal-research.com>
2016-08-05 17:15:33 -0300

    fix output path and columns names

--- data/scripts/interest_spread.py
+++ data/scripts/interest_spread.py
@@ -1,7 +1,8 @@
 ######################################################################
 # Script to download from finnacloud interest rate spread.
 # Initial date: 28/06/2016
-######################################################################
+####################################################################
+
 import bcb
 import json, os
 import pandas as pd
@@ -10,5 +11,6 @@ import pandas as pd
 series = {"1178":"Selic rate", "7801":"cdi1m", "7802": "cdi2m", "7805":"cd16m",
           "7806": "cdi1h"}
 df = bcb.fetch_bcb(series.keys())
-names = [series[n] for n in df.columns]
-df.to_csv('./interest_spread.csv', header=True, index=True)
+df.columns = [series[n] for n in df.columns]
+
+df.to_csv('../interest_spread.csv', header=True, index=True)
