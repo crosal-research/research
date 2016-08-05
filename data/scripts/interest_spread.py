d7cb3fe8213f4de1d352953861cf423037bd02e5
jmrosal <jmrosal@crosal-research.com>
2016-08-05 18:22:07 -0300

    fix initial and end dates when retreiving data

--- data/scripts/interest_spread.py
+++ data/scripts/interest_spread.py
@@ -2,15 +2,18 @@
 # Script to download from finnacloud interest rate spread.
 # Initial date: 28/06/2016
 ####################################################################
-
 import bcb
 import json, os
 import pandas as pd
+from datetime import datetime
 
 
 series = {"1178":"Selic rate", "7801":"cdi1m", "7802": "cdi2m", "7805":"cd16m",
           "7806": "cdi1h"}
-df = bcb.fetch_bcb(series.keys())
+
+today = datetime.today().strftime("%d/%m/%Y")
+
+df = bcb.fetch_bcb(series.keys(), "01/01/2010", today)
 df.columns = [series[n] for n in df.columns]
 
 df.to_csv('../interest_spread.csv', header=True, index=True)
