f07ea8d65c3dc47dd94a4c46bc9398f4fddee077
jmrosal <jmrosal@crosal-research.com>
2016-08-08 12:23:36 -0300

    modify to fetch data from bcb's webservices

--- monetary_policy/interest_spread_chart.py
+++ monetary_policy/interest_spread_chart.py
@@ -2,26 +2,31 @@
 # Chart for interest rates sprads
 # initial date: 28/06/2016
 ######################################################################
-
 import pandas as pd
 import plotly.plotly as py
 import plotly.graph_objs as go
 
 
-df = pd.read_csv("../data/interest_spread.csv", header=0, index_col=0)
-df.columns = ["selic", "cdi6m", 'cdi1y']
-
-start_data = "2016-01-01"
-df_cut = df[df.index >= start_data]
+df = pd.read_csv("../data/interest_spread.csv", header=0, index_col=0,
+                 usecols=[0, 1,4,5]).dropna()
 
 ## chart
-trace01 = go.Scatter(x=df_cut.index, y=df_cut['selic'], name="Selic")
-trace02 = go.Scatter(x=df_cut.index, y=df_cut['cdi6m'], name="6 months DI Rate")
-trace03 = go.Scatter(x=df_cut.index, y=df_cut['cdi1y'], name="1 year DI Rate")
 
-data = [trace01, trace02, trace03]
-layout = go.Layout(title="<b>Interest Rates Term Premium</b>",
-                   yaxis=dict(title="%p.y", tickmode="auto", nticks=5),
-                   font=dict(size=12), legend=dict(x=0, y=-0.4))
-fig = go.Figure(data=data, layout=layout)
-py.image.save_as(fig, '../exhibits/interest_spreads.jpeg', format="jpeg")
+
+def chart_gen(df, title, y_title, leg, data_ini):
+    df_cut = df[df.index >= start_date]
+    data = []
+    for i in range(0, len(df.columns)):
+        trace = go.Scatter(x=df_cut.index, y=df_cut.iloc[:,i], name=leg[i])
+        data.append(trace)
+    layout = go.Layout(title="<b>{}</b>".format(title),
+                       yaxis=dict(title="{}".format(y_title), tickmode="auto", nticks=5),
+                       font=dict(size=18), legend=dict(x=0, y=-0.4))
+    return go.Figure(data=data, layout=layout)
+
+
+#generate chart
+start_date = "2016-01-01"
+py.image.save_as(chart_gen(df, "Interest Rate Term Structure",
+                           "%p.y.", ["Selic", "6M Interbank", "1Y interbank"],
+                           start_date), '../exhibits/interest_spreads.jpeg', format="jpeg")
