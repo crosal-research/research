77615e40fc2136acfc9086b8289dc4c742214e94
jmrosal <jmrosal@crosal-research.com>
2016-08-08 12:21:47 -0300

    modify series to be drawn to labor productivity tfor cost

--- competitiveness/bzusd_productivity.py
+++ competitiveness/bzusd_productivity.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.plotly as py
 import plotly.graph_objs as go
 
-df = pd.read_csv('../data/cut.csv', index_col=0)
+df = pd.read_csv('../data/cut.csv', index_col=0, usecols=[0,1])
 n = [1.0 for i in range(0, df.size)]
 df_ave = pd.DataFrame(n, index=df.index, columns = ['average']) * df.mean()[0]
 df_mid = pd.merge(df, df_ave, left_index=True, right_index=True, how="inner")
@@ -16,12 +16,13 @@ start_date = '1995-01-01'
 df_final = df_mid[df_mid.index >= start_date]
 
 ## chart
-trace01 = go.Scatter(x=df_final.index, y=df_final['cut'], name = "Unit Labor Cost")
+trace01 = go.Scatter(x=df_final.index, y=df_final.iloc[:,0], name = "Labor Productivity")
 trace02 = go.Scatter(x=df_final.index, y=df_final['average'], name = "Average",
                      line=dict(color="red", dash="dot"))
 data = [trace01, trace02]
-layout = go.Layout(title="<b>Unit Labor Cost - Brazil </b>",
-                   font=dict(size=12), legend=dict(x=0, y = -0.2),
+layout = go.Layout(title="<b> Deflated Labor Productivity in USD - Brazil </b>",
+                   font=dict(size=18), legend=dict(x=0, y = -0.2),
                    yaxis=dict(title="pts", tickmode="auto", nticks=5))
 fig = go.Figure(data=data, layout=layout)
-py.image.save_as(fig, '../exhibits/cut.jpeg', format="jpeg")
+py.image.save_as(fig, '../exhibits/usdbr_productivity.jpeg',
+                 format="jpeg")
