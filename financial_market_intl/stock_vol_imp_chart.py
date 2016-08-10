bc81ccd287e20ad435b66baf8b85226d1d0b15a8
jmrosal <jmrosal@crosal-research.com>
2016-08-10 17:08:31 -0300

    add Brexit line to the chart

--- financial_market_intl/stock_vol_imp_chart.py
+++ financial_market_intl/stock_vol_imp_chart.py
@@ -10,8 +10,8 @@ import numpy as np
 df = pd.read_csv("../data/stock_vol_imp.csv", header=0, index_col=0, na_values = ["."])
 
 ## vixes
-date_new = "2014-01-01"
-df_new = df[df.index >= date_new]
+date_new = "2015-01-01"
+df_new = df[df.index >= date_new].dropna()
 
 trace01 = go.Scatter(x=df_new.index, y = df_new['VIXCLS'], name = "VIX")
 trace02 = go.Scatter(x=df_new.index, y = df_new['VXEEMCLS'], name = "VOL - EM")
@@ -20,6 +20,22 @@ data = [trace01, trace02, trace03]
 layout = go.Layout(title="<b>Implicit Vol - Stock Markets</b>",
                    yaxis=dict(title="pts", tickmode="auto", nticks=5),
                    font=dict(size=18), legend=dict(x=0, y=-0.4))
+
+brexit = "2016-06-23"
+layout.update(dict(
+    shapes=[
+        {"type": 'line',
+         'xref': 'x',
+         'yref': 'y',
+         'x0': brexit,
+         'y0': 0,
+         'x1': brexit,
+         'y1': df_new['VXEWZCLS'].max()+5,
+         'line': dict(dash="dot", color="blue")
+        }],
+    annotations=[go.Annotation(text="Brexit", x=brexit,
+                               y=df_new["VXEWZCLS"].max())]))
+
 fig = go.Figure(data=data, layout=layout)
 py.image.save_as(fig, "../exhibits/stock_vol_imp.jpeg", format="jpeg")
 
