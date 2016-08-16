f098bd1b03f21734ea54fe194d33b59828cbb67d
jmrosal <jmrosal@crosal-research.com>
2016-08-15 21:10:58 -0300

    fix markets and lines' colors

--- us/us_cpi_chart.py
+++ us/us_cpi_chart.py
@@ -15,12 +15,15 @@ df_year.columns = ["CPI", "Core Inflation"]
 def gen_chart(df, title, y_title, leg, date_ini):
     df_final = df[df.index >= date_ini]
     data = []
+    col = ["#00cc00", "#ff9900"]
     for i in range(0, len(df.columns)):
-        data.append(go.Scatter(x=df_final.index, y=df_final.iloc[:, i], name=leg[i]))
+        data.append(go.Scatter(x=df_final.index, y=df_final.iloc[:, i], name=leg[i],
+                    marker=dict(color=col[i])))
         data.append(go.Scatter(x=df_final.tail(1).index, y=df_final.tail(1).iloc[:,i],
                     mode="markers+text",
                     text=["<b>"+str(round(df_final.tail(1).iloc[:, i].values[0], 1))+"</b>"],
-                    textposition='top', showlegend=False))
+                               textposition='top', showlegend=False,
+                    marker=dict(color=col[i])))
     layout = go.Layout(title="<b>{}</b>".format(title),
                        yaxis=dict(title=y_title), font=dict(size=18),
                        legend=dict(x=0, y=-0.4))
