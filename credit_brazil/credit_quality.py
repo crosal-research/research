cfb7cd7c81ca0fbd0e12e921bb9babb8ef83d4a8
jmrosal <jmrosal@crosal-research.com>
2016-08-16 14:28:09 -0300

    add decoration to charts

--- credit_brazil/credit_quality.py
+++ credit_brazil/credit_quality.py
@@ -11,23 +11,32 @@ df = pd.read_csv("../data/credit_quality.csv", header=0, index_col=0)
 
 
 ## function to produce charts
-def gen_chart(df, title, y_title, leg, date_ini):
+def gen_chart(df, title, y_title, date_ini):
     """"""
     df_final = df[df.index >= date_ini]
     col = ["#000099", " #ff9900", "#00cc00"]
     data = []
     for i in range(0, len(df.columns)):
         data.append(go.Scatter(x=df_final.index, y=df_final.iloc[:, i],
-                    name=leg[i], marker=dict(color=col[i])))
+                               name=df.columns[i],
+                               marker=dict(color=col[i])))
         data.append(go.Scatter(x=df_final.tail(1).index, y=df_final.tail(1).iloc[:, i],
                                marker=dict(color=col[i]), showlegend=False,
                                mode="markers+text",
                                text=["<b>"+str(round(df_final.tail(1).iloc[:, i].values[0], 1))+"</b>"],
-                               textposition='top'))
+                               textposition='top',
+                               textfont=dict(size=14)))
 
     layout = go.Layout(title="<b>{}</b>".format(title),
                        yaxis=dict(title=y_title, tickmode="auto", nticks=5),
-                       legend=dict(x=0, y=-0.4))
+                       legend=dict(x=0, y=-0.4),
+                       font=dict(size=18),
+                       annotations=[dict(x=df.tail(1).index.values[0],
+                                         y=df.max().max()*(1.1),
+                                         xref='x',
+                                         yref='y',
+                                         text="<b>"+ pd.to_datetime(df.tail(1).index).strftime("%b-%Y")[0]+"</b>",
+                                         font=dict(size=14))])
     return go.Figure(data=data, layout=layout)
 
 
@@ -36,7 +45,9 @@ df_qual = df.loc[:, ['total_del', 'companies_del', 'households_del']]
 df_qual.columns = ["Total", "Companies", "Households"]
 date_qual = '2012-01-01'
 py.image.save_as(gen_chart(df_qual, "Deliquencies", "%Credit Class",
-                           df_qual.columns, date_qual ),"../exhibits/credit_quality.jpeg",format="jpeg")
+                           date_qual),
+                 "../exhibits/credit_quality.jpeg",
+                 format="jpeg")
 
 
 ## chart on interest rate
@@ -44,6 +55,6 @@ df_int = df.loc[:, ['total_juros', 'companies_juros', 'households_juros']]
 df_int.columns = ["Total", "Companies", "Households"]
 date_int = '2012-01-01'
 py.image.save_as(gen_chart(df_int, "Interest Rates", "%Credit Class",
-                           df_int.columns, date_int),
+                           date_int),
                  "../exhibits/credit_interest.jpeg",
                  format="jpeg")
--- credit_brazil/credit_volume.py
+++ credit_brazil/credit_volume.py
@@ -12,22 +12,56 @@ df_12m = df.rolling(window=12).sum().dropna()
 df_gdp = df_12m.div(df_12m['GDP'], axis=0).iloc[:,1:]*10
 
 
-def credit_volume(df_gdp, new_date, title):
-    df_gdp_new = df_gdp[df_gdp.index >= new_date]
-    names = df_gdp.columns
-    ## volume
-    trace01 = go.Scatter(x=df_gdp_new.index, y=df_gdp_new['Total'],
-                         name=names[0])
-    trace02 = go.Scatter(x=df_gdp_new.index, y=df_gdp_new['Institutions'],
-                         name=names[1])
-    trace03 = go.Scatter(x=df_gdp_new.index, y=df_gdp_new['Households'],
-                         name=names[2])
-    data0 = [trace01, trace02, trace03]
-    layout0 = go.Layout(title="<b>{}</b>".format(title),
-                        yaxis=dict(title="%GDP", tickmode="auto", nticks=5),
-                        font=dict(size=18),
-                        legend=dict(x=0, y=-0.4))
-    return go.Figure(data=data0, layout=layout0)
 
-py.image.save_as(credit_volume(df_gdp, "2010-01-01", "Credit Volume - Brazil" ),
+def gen_chart(df, title, y_title, date_ini, source=True):
+    df_final = df[df.index >= date_ini]
+    col = ["#000099", " #ff9900", "#00cc00"]
+    data = []
+    for i in range(0, len(df.columns)):
+        data.append(go.Scatter(x=df_final.index, y=df_final.iloc[:, i],
+                               name=df.columns[i],
+                               marker=dict(color=col[i])))
+        data.append(go.Scatter(x=df_final.tail(1).index, y=df_final.tail(1).iloc[:, i],
+                               marker=dict(color=col[i]), showlegend=False,
+                               mode="markers+text",
+                               text=["<b>"+str(round(df_final.tail(1).iloc[:, i].values[0], 1))+"</b>"],
+                               textposition='top left',
+                               textfont=dict(size=14)))
+
+    layout = go.Layout(title="<b>{}</b>".format(title),
+                       yaxis=dict(title=y_title, tickmode="auto", nticks=5),
+                       legend=dict(x=0, y=-0.4),
+                       font=dict(size=18),
+                       annotations=[dict(x=df.tail(1).index.values[0],
+                                         y=df.max().max()*(1.1),
+                                         xref='x',
+                                         yref='y',
+                                         text="<b>"+ pd.to_datetime(df.tail(1).index).strftime("%b-%Y")[0]+"</b>",
+                                         font=dict(size=14))])
+    if source == True:
+        layout.update(dict(annotations=[dict(
+            x=.95,
+            y=-0.4,
+            xref='paper',
+            yref='paper',
+            text="<b><i>CRosal Independent Research</i></b>",
+            font=dict(size=14, family='Courier new', color="#ffffff"),
+            bgcolor='#ff8080',
+            opcity=0.5,
+            showarrow=False
+        )]))
+
+    return go.Figure(data=data, layout=layout)
+
+
+## Generate chart for households, companies and Total
+start_date = "2000-01-01"
+py.image.save_as(gen_chart(df_gdp.loc[:,['Total', "Institutions", "Households"]]
+                           , "Credit Volume - Brazil", "%GDP", start_date),
                  '../exhibits/credit_gdp_chart.jpeg', format="jpeg")
+
+## Generate chart for earmarked, non-earmarked and Total
+start_date = "2000-01-01"
+py.image.save_as(gen_chart(df_gdp.loc[:,['Total', "non-earmarked", "earmarked"]]
+                           , "Credit Volume - Brazil", "%GDP", start_date),
+                 '../exhibits/credit_gdp_earmarked.jpeg', format="jpeg")
