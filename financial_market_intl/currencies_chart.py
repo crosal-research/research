381023c810cc339934586bc68c4dc8c2291d39a8
jmrosal <jmrosal@crosal-research.com>
2016-08-29 15:34:19 -0300

    simple refactoring

--- financial_market_intl/currencies_chart.py
+++ financial_market_intl/currencies_chart.py
@@ -8,18 +8,30 @@ import plotly.graph_objs as go
 import numpy as np
 
 df = pd.read_csv("../data/currencies.csv", index_col=[0])
-
+curs = [u'Australian Dollar', u'Brazilian Real', u'Indian Ruppee',
+        u'Russian Ruble', u'Turkish Lira', u'Canadian Dollar',
+        u'New Zealand Dollar', u'South African Rand']
+df_curs = df.loc[:, curs]
 
 brexit = "2016-06-22"
 
 def chart_gen(df, leg, date_ini): #, title, y_title, leng, date_ini):
-    df_final = df[df.index >= date_ini] / df[df.index==date_ini].iloc[0]
+    df_final = (df[df.index >= date_ini] / df[df.index==date_ini].iloc[0] -1)*100
     data = []
     for i in range(len(df.columns)):
-        dat = go.Scatter(x=df_final.index, y=df_final.iloc[:,i], name=leg[i])
-        data.append(dat)
-    layout = go.Layout(title="whatever")
+        if leg[i] == 'Brazilian Real':
+            dat = go.Scatter(x=df_final.index, y=df_final.iloc[:,i], name=leg[i],
+                             line=dict(width=4))
+            data.append(dat)
+        else:
+            dat = go.Scatter(x=df_final.index, y=df_final.iloc[:,i], name=leg[i])
+            data.append(dat)
+    layout = go.Layout(title="<b>Commodity Producers and EM Currencies </b> </br> (since Brexit)",
+                       font=dict(size=18),
+                       yaxis=dict(tickmode='auto', nticks=5, tickfont=dict(size=14)),
+                       xaxis=dict(tickfont=dict(size=14)),
+                       legend=dict(y=0.3, font=dict(size=14)))
     return go.Figure(data=data, layout=layout)
 
-py.image.save_as(chart_gen(df, df.columns, brexit), "../exhibits/currencies.jpeg",
+py.image.save_as(chart_gen(df_curs, df_curs.columns, brexit), "../exhibits/currencies.jpeg",
                  format="jpeg")
