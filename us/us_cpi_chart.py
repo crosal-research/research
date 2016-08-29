5386baad12b6f344ad05a1b5fd7f8aa652866684
jmrosal <jmrosal@crosal-research.com>
2016-08-29 15:35:55 -0300

    add inflation target line

--- us/us_cpi_chart.py
+++ us/us_cpi_chart.py
@@ -9,20 +9,22 @@ import plotly.graph_objs as go
 
 df = pd.read_csv("../data/cpi_us.csv", index_col=[0]).dropna()
 df_year = df.pct_change(periods=12)*100
-df_year.columns = ["CPI", "Core Inflation"]
+df_year['Target'] = 2.0
+df_year.columns = ["CPI", "Core Inflation", "Target"]
 
 
 def gen_chart(df, title, y_title, leg, date_ini):
     df_final = df[df.index >= date_ini]
     data = []
-    col = ["#00cc00", "#ff9900"]
+    col = ["#00cc00", "#ff9900", "black"]
     for i in range(0, len(df.columns)):
+        pos = 'top' if i != 1 else 'bottom-right'
         data.append(go.Scatter(x=df_final.index, y=df_final.iloc[:, i], name=leg[i],
                     marker=dict(color=col[i])))
         data.append(go.Scatter(x=df_final.tail(1).index, y=df_final.tail(1).iloc[:,i],
                     mode="markers+text",
                     text=["<b>"+str(round(df_final.tail(1).iloc[:, i].values[0], 1))+"</b>"],
-                               textposition='top', showlegend=False,
+                               textposition=pos, showlegend=False,
                     marker=dict(color=col[i])))
     layout = go.Layout(title="<b>{}</b>".format(title),
                        yaxis=dict(title=y_title), font=dict(size=18),
