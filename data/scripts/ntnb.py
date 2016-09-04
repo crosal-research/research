b5ab6cb9e77c1ffb07c4a61416d34274f8f20780
jmrosal <jmrosal@crosal-research.com>
2016-09-04 17:07:13 -0300

    add script to fetch financial info about ntnb

---
+++ data/scripts/ntnb.py
@@ -0,0 +1,12 @@
+######################################################################
+# script to fetch information on ntn-b from Brazilian Treasry
+# initial date: 04/09/2016
+######################################################################
+import pandas as pd
+
+url = "http://sisweb.tesouro.gov.br/apex/COSIS_SISTD.obtem_arquivo?p_id=251:82537"
+
+df = pd.read_excel(url, 2, skiprows = [0, 1], index_col = 0,
+                   names = ["Bid yield", "Offer yield",
+                            "Bid price", "Offer Price", "Base price"])
+df.to_csv("../ntnb.csv", header=True, index=True)
